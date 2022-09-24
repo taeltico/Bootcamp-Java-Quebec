916f032fbf32633e1c0ec92e0f6af5aa76ac1196
Rafael Wanderley <Rafaelwvs16@icloud.com>
2022-09-24 06:45:00 -0300

    first commit

---
+++ src/MODEL/Gato.java
@@ -0,0 +1,40 @@
+package MODEL;
+
+public class Gato() {
+
+
+    /*private string nome;
+    private string cor;
+    private integer idade;
+
+    public Gato(string nome, string cor, integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }*/
+
+   /* @Override
+    public String toString() {
+        return "Gato{" +
+                "nome=" + nome +
+                ", cor=" + cor +
+                ", idade=" + idade +
+                '}';
+    }
+
+    public void setNome(string nome) {
+        this.nome = nome;
+    }
+
+    public void setCor(string cor) {
+        this.cor = cor;
+    }
+
+    public void setIdade(integer idade) {
+        this.idade = idade;
+    }
+
+    public Gato() {
+    }
+    */
+}
