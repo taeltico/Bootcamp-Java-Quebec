fb2f4a32fdc92764181348b93f0e069fa68ea23d
Rafael Wanderley <Rafaelwvs16@icloud.com>
2022-09-24 06:59:11 -0300

    Atualização

--- src/br/com/dio/primeiro_programa.java
+++ src/br/com/dio/primeiro_programa.java
@@ -4,6 +4,7 @@ import MODEL.Gato;
 
 public class primeiro_programa {
     public static void main(String[] args) {
+        Gato New;
         Gato gato = New Gato();
         System.out.println(gato);
 
