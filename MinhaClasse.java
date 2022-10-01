6c8807b95d5b6dc84c614d0bc44eed02d24dd41b
˜Rafael <rafaelwvs166@gmail.com>
2022-10-01 16:43:14 -0300

    ˜javas˜

--- MinhaClasse.java
+++ MinhaClasse.java
@@ -1,8 +1,14 @@
 public class MinhaClasse {
 
-public static void main(String[] args) {
+public static void main (String[] args) {
+    system.out.print (˜Ola! Mundo Fugindo da maldicao do ola mundo˜);
+    int ano = 2021;
+    \
+    int ano = 2022;
+    final string BR = ˜Brasil˜;
     
-    System.out.print (˜Ola! Mundo Fugindo da maldicao do ola mundo˜);
+    BR = ˜Brazil˜;
+    
 }
 
 }
